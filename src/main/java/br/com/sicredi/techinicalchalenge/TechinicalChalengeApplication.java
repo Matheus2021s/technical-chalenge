58478d902605e1bab6011f113d2ec8453889c05d
Matheus2021s <matheusmartinsdacosta21@gmail.com>
2022-03-10 11:54:03 -0300

    Initial commit

---
+++ src/main/java/br/com/sicredi/techinicalchalenge/TechinicalChalengeApplication.java
@@ -0,0 +1,13 @@
+package br.com.sicredi.techinicalchalenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechinicalChalengeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TechinicalChalengeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/sicredi/techinicalchalenge/TechinicalChalengeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.sicredi.techinicalchalenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechinicalChalengeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
